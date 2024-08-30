7c0675220a796acf6f3081153aa3ba34cc8f2b47
Victor Doyle <victor-doyle@outlook.com>
2024-08-29 23:01:04 -0400

    show header reset during dev build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />  {/* Home n basic welcome */}
+      {/* TODO: For prod, all screens `headerShown: false` but for now keep during build */}
+      <Stack.Screen name="index" options={{ title: 'Home' }} />  {/* Home n basic welcome */}
       <Stack.Screen name="auth" options={{ title: 'Authentication' }} /> {/* Auth Screen login/signup*/}
     </Stack>
   );
